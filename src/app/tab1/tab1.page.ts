878260f697234d10aa96235cdef217486e8dc796
千本達也 <ts7189@koyama-kk.co.jp>
2022-01-25 13:06:27 +0900

    add ion-datetime mode=ios and mode=md

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,20 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  public isPopover = false;
+  public isPopover1 = false;
+  public isPopover2 = false;
 
   constructor() {}
 
-  openPopover() {
-    if (this.isPopover) {
-      this.isPopover = false;
-    } else {
-      this.isPopover = true;
-    }
+  openPopover1() {
+    this.isPopover1 = this.isPopover1 ? false : true;
   }
 
+  openPopover2() {
+    this.isPopover2 = this.isPopover2 ? false : true;
+  }
 }
