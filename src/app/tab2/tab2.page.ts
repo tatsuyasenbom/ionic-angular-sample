f91c12337cd05c3e44844f80a7cf5fc4a73bb643
ts7189 <ts7189@koyama-kk.co.jp>
2022-08-25 11:52:34 +0900

    add barcode scanner

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  styles: [
+    `
+      .scan-result {
+        background-color: #eee;
+        padding: 1em;
+        font-size: 2em;
+      }
+    `,
+  ],
+  providers: [BarcodeScanner],
 })
 export class Tab2Page {
+  scannedText: string;
+  format: string;
 
-  constructor() {}
+  constructor(public barcodeScanner: BarcodeScanner) {}
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ScanPage');
+  }
+
+  scan() {
+    this.barcodeScanner
+      .scan()
+      .then((barcodeData) => {
+        if (barcodeData.cancelled) {
+          this.setValues('', '(cancelled)');
+        } else {
+          this.setValues(
+            barcodeData.text || '???',
+            barcodeData.format || '???'
+          );
+        }
+      })
+      .catch((err) => {
+        this.setValues(`Error ${err}`, '');
+      });
+  }
+
+  clear() {
+    this.setValues('', '');
+  }
+
+  setValues(s, f) {
+    this.scannedText = s;
+    this.format = f;
+  }
 }
